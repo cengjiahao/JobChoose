d603cae5d8f80feaf6183bb6896b59a486df6a4b
ZJH <1290248303@qq.com>
2018-12-14 10:22:14 +0800

    1

---
+++ JobChoose/JobChoose/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JobChoose
+//
+//  Created by ZJH on 2018/12/14.
+//  Copyright © 2018年 ZJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JobChoose/JobChoose/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JobChoose
+//
+//  Created by ZJH on 2018/12/14.
+//  Copyright © 2018年 ZJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
